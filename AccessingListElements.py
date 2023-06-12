d2404e4f1f835059a01627a5dd8890bc45145360
JayaSamuthraDevi <115087700+JayaSamuthraDevi@users.noreply.github.com>
2023-06-12 10:14:20 +0530

    Add files via upload

---
+++ AccessingListElements.py
@@ -0,0 +1,14 @@
+
+
+list1=["a","b","c","d"]
+
+#python allows -ve indexing
+print(list1[-1])
+
+
+#to print set of elements
+print(list1[1:3])
+
+
+list2=[1,2,3,"a",["abc","cde"]]
+print(list2[4][0])#double indexing to access element of list inside a list
